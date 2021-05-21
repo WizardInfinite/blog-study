fb87446cc5423b501a8fd0f9e7c644ee63d4ab18
hm <23221323@qq.com>
2021-05-21 17:22:53 +0800

    第二次提交

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -26,7 +26,6 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto" v-html="$page.post.content">
-           
           </div>
         </div>
       </div>
